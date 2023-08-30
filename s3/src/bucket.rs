81b6f1721463a43a24d0798266eb4108e8081501
Jim Crossley <jim@crossleys.org>
2023-08-30 19:58:10 -0400

    fix doc tests after cherry picking PR #343
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- s3/src/bucket.rs
+++ s3/src/bucket.rs
@@ -137,9 +137,10 @@ impl Bucket {
     ///    "response-content-disposition".into(),
     ///    "attachment; filename=\"test.png\"".into(),
     /// );
-    ///
-    /// let url = bucket.presign_get("/test.file", 86400, Some(custom_queries)).unwrap();
-    /// println!("Presigned url: {}", url);
+    /// async {
+    ///   let url = bucket.presign_get("/test.file", 86400, Some(custom_queries)).await.unwrap();
+    ///   println!("Presigned url: {}", url);
+    /// };
     /// ```
     pub async fn presign_get<S: AsRef<str>>(
         &self,
@@ -176,9 +177,10 @@ impl Bucket {
     /// let bucket = Bucket::new(bucket_name, region, credentials).unwrap();
     ///
     /// let post_policy = "eyAiZXhwaXJhdGlvbiI6ICIyMDE1LTEyLTMwVDEyOjAwOjAwLjAwMFoiLA0KICAiY29uZGl0aW9ucyI6IFsNCiAgICB7ImJ1Y2tldCI6ICJzaWd2NGV4YW1wbGVidWNrZXQifSwNCiAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAidXNlci91c2VyMS8iXSwNCiAgICB7ImFjbCI6ICJwdWJsaWMtcmVhZCJ9LA0KICAgIHsic3VjY2Vzc19hY3Rpb25fcmVkaXJlY3QiOiAiaHR0cDovL3NpZ3Y0ZXhhbXBsZWJ1Y2tldC5zMy5hbWF6b25hd3MuY29tL3N1Y2Nlc3NmdWxfdXBsb2FkLmh0bWwifSwNCiAgICBbInN0YXJ0cy13aXRoIiwgIiRDb250ZW50LVR5cGUiLCAiaW1hZ2UvIl0sDQogICAgeyJ4LWFtei1tZXRhLXV1aWQiOiAiMTQzNjUxMjM2NTEyNzQifSwNCiAgICB7IngtYW16LXNlcnZlci1zaWRlLWVuY3J5cHRpb24iOiAiQUVTMjU2In0sDQogICAgWyJzdGFydHMtd2l0aCIsICIkeC1hbXotbWV0YS10YWciLCAiIl0sDQoNCiAgICB7IngtYW16LWNyZWRlbnRpYWwiOiAiQUtJQUlPU0ZPRE5ON0VYQU1QTEUvMjAxNTEyMjkvdXMtZWFzdC0xL3MzL2F3czRfcmVxdWVzdCJ9LA0KICAgIHsieC1hbXotYWxnb3JpdGhtIjogIkFXUzQtSE1BQy1TSEEyNTYifSwNCiAgICB7IngtYW16LWRhdGUiOiAiMjAxNTEyMjlUMDAwMDAwWiIgfQ0KICBdDQp9";
-    ///
-    /// let url = bucket.presign_post("/test.file", 86400, post_policy.to_string()).unwrap();
-    /// println!("Presigned url: {}", url);
+    /// async {
+    ///   let url = bucket.presign_post("/test.file", 86400, post_policy.to_string()).await.unwrap();
+    ///   println!("Presigned url: {}", url);
+    /// };
     /// ```
     pub async fn presign_post<S: AsRef<str>>(
         &self,
@@ -221,9 +223,10 @@ impl Bucket {
     ///    HeaderName::from_static("custom_header"),
     ///    "custom_value".parse().unwrap(),
     /// );
-    ///
-    /// let url = bucket.presign_put("/test.file", 86400, Some(custom_headers)).unwrap();
-    /// println!("Presigned url: {}", url);
+    /// async {
+    ///   let url = bucket.presign_put("/test.file", 86400, Some(custom_headers)).await.unwrap();
+    ///   println!("Presigned url: {}", url);
+    /// };
     /// ```
     pub async fn presign_put<S: AsRef<str>>(
         &self,
@@ -256,9 +259,10 @@ impl Bucket {
     /// let region = "us-east-1".parse().unwrap();
     /// let credentials = Credentials::default().unwrap();
     /// let bucket = Bucket::new(bucket_name, region, credentials).unwrap();
-    ///
-    /// let url = bucket.presign_delete("/test.file", 86400).unwrap();
-    /// println!("Presigned url: {}", url);
+    /// async {
+    ///   let url = bucket.presign_delete("/test.file", 86400).await.unwrap();
+    ///   println!("Presigned url: {}", url);
+    /// };
     /// ```
     pub async fn presign_delete<S: AsRef<str>>(
         &self,
